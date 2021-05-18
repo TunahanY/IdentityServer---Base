0f70d9c2b04042bfe9220a5a197aadee917a705e
Tunahan Yollar <mtunahanyollar@gmail.com>
2021-05-19 01:46:37 +0300

    Add project files.

---
+++ IdentityBased.Client1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityBased.Client1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
